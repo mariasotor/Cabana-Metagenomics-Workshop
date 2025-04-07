bd0132ac799b1b66577577914a6abc9b69446d12
M. Alejandra Soto <34344427+mariasotor@users.noreply.github.com>
2025-04-06 21:28:48 -0500

    Update rename_bins.sh

--- helper_scripts/rename_bins.sh
+++ helper_scripts/rename_bins.sh
@@ -1,9 +1,6 @@
 
 #!/bin/bash
 
-# Define the parent directory
-parent_folder="path/to/refinement/folder"
-
 # Loop through each sample ID folder
 for sample_folder in "$parent_folder"/*/; do
     # Extract the sample ID (folder name)
@@ -16,11 +13,11 @@ for sample_folder in "$parent_folder"/*/; do
     if [[ -d "$bins_folder" ]]; then
         # Loop through .fa files in the metawrap_50_5_bins folder
         for file in "$bins_folder"/*.fa; do
-            # Extract the bin identifier (bin.1, bin.2, etc.)
-            bin_id=$(basename "$file" | sed -E 's/(bin\.[0-9]+)\..*\.fa/\1/')
+            # Extract the bin identifier (bin.1, bin.2, etc.), keeping the .fa
+            bin_id=$(basename "$file")
 
-            # Define the new filename
-            new_name="${sample_id}_${bin_id}.fa"
+            # Define the new filename (without appending .fa)
+            new_name="${sample_id}_${bin_id}"
 
             # Rename the file
             mv "$file" "$bins_folder/$new_name"
